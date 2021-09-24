2ebdae7d8769daab47a18e72576113d9b66d7abc
soshiharami <soshi0807sss@gmail.com>
2021-09-25 02:52:30 +0900

    feat: set up docker

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { app } from './app'
 import { envs } from './helpers/utils'
 
 const start = async () => {
-  await app.listen(envs.PORT)
+  await app.listen(envs.PORT, '0.0.0.0')
   app.log.info(`app running on ${envs.HOST}:${envs.PORT}/`)
 }
 
