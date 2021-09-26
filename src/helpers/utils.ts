b814a057bfbeb878d1bf797f7a98966f3579010e
soshiharami <soshi0807sss@gmail.com>
2021-09-26 15:59:19 +0900

    fix: deplpy

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -7,7 +7,7 @@ export const isDev = () => process.env.NODE_ENV === 'development'
 export const envs = {
   PORT: process.env.PORT || 4000,
   HOST: process.env.HOST || 'http://localhost',
-  CORS_HOST: process.env.CORS_HOST || 'http://localhost:3000/',
+  CORS_HOST: process.env.CORS_HOST || '*',
   JWT_SECRET: process.env.JWT_SECRET || 'secret123',
 }
 
